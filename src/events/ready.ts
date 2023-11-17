65564884f98ce0fabf8bac07f2fae634f3027f8d
kiidii <kiidii@aniname.com>
2023-11-17 23:33:33 +0200

    try to log guilds

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -11,6 +11,8 @@ export default async function ready (client: Client) {
   const guild = client.guilds.cache.first()
   const channel = client.channels.cache.get(BOT_CHANEL_ID as string)
 
+  console.log('client.guilds', client.guilds)
+
   if (!channel) throw new Error(`Channel not found!`)
   if (!guild) throw new Error(`Guild not found!`)
 
