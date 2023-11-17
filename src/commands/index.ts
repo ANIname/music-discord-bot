4dff084d4d88e274054ddb206a3e500ac72a31c6
kiidii <kiidii@aniname.com>
2023-11-17 22:36:12 +0200

    disable commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,17 +1,17 @@
-import { directoryImport } from 'directory-import'
+// import { directoryImport } from 'directory-import'
 
-import { Command, Commands } from './types.d'
+import { Commands } from './types.d'
 
 const commands: Commands = {}
 
 // Import all commands from the commands directory and add them to the commands object
 // Example: ./commands/test/ping.ts => commands['ping'] = { data: { ... }, execute: () => { ... } }
-directoryImport((moduleName, _, moduleData) => {
-  if (moduleName === 'index') return
+// directoryImport((moduleName, _, moduleData) => {
+//   if (moduleName === 'index') return
 
-  const { data, execute } = moduleData as Command
+//   const { data, execute } = moduleData as Command
 
-  commands[data.name] = { data, execute }
-})
+//   commands[data.name] = { data, execute }
+// })
 
 export default commands
